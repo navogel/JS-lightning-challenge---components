acc60d9e610e9cc261f410d7a77b9c9566b57933
Nate V <navogel@gmail.com>
2019-08-28 09:42:39 -0500

    added functions to manipulate the DOM

--- js/main.js
+++ js/main.js
@@ -25,3 +25,24 @@ let eggs = {
 const breakfastArray = [cereal, fruit, eggs];
 
 console.log(breakfastArray);
+
+const breakfastContainer = document.querySelector("#breaky");
+
+console.log(breakfastContainer);
+
+function breakfast(array) {
+	return `
+    <div>
+        <h1>${array.name}</h1>
+        <p>Cold? ${array.cold} </p>
+        <p> Calories: ${array.calories}</p>
+    </div>
+
+    `;
+}
+
+breakfastArray.forEach(element => {
+	breakfastContainer.innerHTML += breakfast(element);
+});
+
+// breakfast(breakfastArray);
