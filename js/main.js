b40118218650033f5cc742298e97b5e40faabda0
Nate V <navogel@gmail.com>
2019-08-28 09:54:55 -0500

    added comments

--- js/main.js
+++ js/main.js
@@ -26,14 +26,18 @@ const breakfastArray = [cereal, fruit, eggs];
 
 console.log(breakfastArray);
 
+//create container for component
+
 const breakfastContainer = document.querySelector("#breaky");
 
 console.log(breakfastContainer);
 
+//function that returns element contents
+
 function breakfast(array) {
 	return `
     <div>
-        <h1 style="color: blue; text-align: center">${array.name}</h1>
+        <h1 style="color: black; text-align: center">${array.name}</h1>
         <p style="color: aqua; text-align: center">Cold? ${array.cold} </p>
         <p style="color: red; text-align: center"> Calories: ${array.calories}</p>
     </div>
@@ -41,8 +45,8 @@ function breakfast(array) {
     `;
 }
 
+//loop that adds array contents to component
+
 breakfastArray.forEach(element => {
 	breakfastContainer.innerHTML += breakfast(element);
 });
-
-// breakfast(breakfastArray);
