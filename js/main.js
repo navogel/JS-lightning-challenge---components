8872a675e3d6b5d1e732cb9e3dea5f74b7d44586
Nate V <navogel@gmail.com>
2019-08-28 09:24:31 -0500

    added array of objects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 console.log("hello");
+
+
+// create breakfast objects
+
+let cereal = {
+    name: "lucky charms"
+    cold: true,
+    calories: 300,
+
+}
+
+let fruit = {
+    name: "apple",
+    cold: true,
+    calories: 95
+}
+
+let eggs = {
+    name: "eggs"
+    cold: false,
+    calories: 150
+}
+
+//breakfast array of objects
+
