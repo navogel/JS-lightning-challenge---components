e624e29288dd1ab9c4e2552393e31ab15e046b35
Nate V <navogel@gmail.com>
2019-08-28 09:26:23 -0500

    fixed errors in code

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 console.log("hello");
 
-
 // create breakfast objects
 
 let cereal = {
-    name: "lucky charms"
-    cold: true,
-    calories: 300,
-
-}
+	name: "lucky charms",
+	cold: true,
+	calories: 300
+};
 
 let fruit = {
-    name: "apple",
-    cold: true,
-    calories: 95
-}
+	name: "apple",
+	cold: true,
+	calories: 95
+};
 
 let eggs = {
-    name: "eggs"
-    cold: false,
-    calories: 150
-}
+	name: "eggs",
+	cold: false,
+	calories: 150
+};
 
 //breakfast array of objects
 
-const breakfastArray = [cereal, fruit, eggs]
+const breakfastArray = [cereal, fruit, eggs];
 
+console.log(breakfastArray);
